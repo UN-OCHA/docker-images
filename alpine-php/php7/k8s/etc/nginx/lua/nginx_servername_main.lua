8b3ef863cde87185209af604372969df8d874bd2
Peter Lieverdink <lieverdink@reliefweb.int>
2020-01-27 09:45:25 +1100

    [OPS-6164] Use a comma as hostname separator to match LE proxy.
    Take this opportunity to strip spaces and quotes. Hostnames do not
    container either and we *could* end up with them, as docker-compose
    adds the quotes to the variable if it is set via a .env file.

--- alpine-php/php7/k8s/etc/nginx/lua/nginx_servername_main.lua
+++ alpine-php/php7/k8s/etc/nginx/lua/nginx_servername_main.lua
@@ -1,6 +1,6 @@
 ngx.ctx.host_name_list = {}
 for name in os.getenv("NGINX_SERVERNAME"):gmatch("[^,]+") do
-  name = name:gsub("%s+", ""):lower()
+  name = name:gsub("\"", ""):gsub("%s+", ""):lower()
   table.insert(ngx.ctx.host_name_list, name)
 end
 return ngx.ctx.host_name_list[1]
