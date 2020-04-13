f469f66c740c3977705dab38c32feed06f56bfc1
orakili <github@orakili.net>
2020-04-13 09:07:40 +0900

    [OPS-6379] Disable forced https redirection on hostname basis in php k8s image (#241)

--- alpine-php/php7/k8s/etc/nginx/lua/nginx_https_redir.lua
+++ alpine-php/php7/k8s/etc/nginx/lua/nginx_https_redir.lua
@@ -1,8 +1,11 @@
 if ngx.var.http_x_forwarded_proto == "https" then
   return 0
 end
-ngx.ctx.override_protocol_redirect = os.getenv("NGINX_OVERRIDE_PROTOCOL")
-if ngx.ctx.override_protocol_redirect == "HTTP" then
-  return 0
+http_host = ngx.var.http_host:lower()
+for name in os.getenv("NGINX_OVERRIDE_PROTOCOL"):gmatch("[^,]+") do
+  name = name:gsub("\"", ""):gsub("%s+", ""):lower()
+  if name == http_host or name == "HTTP" then
+    return 0
+  end
 end
 return 1
