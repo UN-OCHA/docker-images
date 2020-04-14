a01ac5648593a27c97c61b567737f9922c4ef314
Peter Lieverdink <lieverdink@reliefweb.int>
2020-04-14 14:48:47 +1000

    [OPS-6379] Be nice when no HTTPS redirect exceptions are defined.
    Do also just comment our code. That is nice for future $me and future $you.

--- alpine-php/php7/k8s/etc/nginx/lua/nginx_servername_main.lua
+++ alpine-php/php7/k8s/etc/nginx/lua/nginx_servername_main.lua
@@ -1,6 +1,9 @@
+-- Initialise a new internal variable for the defined hostnames.
 ngx.ctx.host_name_list = {}
+-- Loop through the environment variable and make a list of allowed hostnames.
 for name in os.getenv("NGINX_SERVERNAME"):gmatch("[^,]+") do
   name = name:gsub("\"", ""):gsub("%s+", ""):lower()
   table.insert(ngx.ctx.host_name_list, name)
 end
+-- Return the first (= main) defined hostname.
 return ngx.ctx.host_name_list[1]
