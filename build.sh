f7576c3baab2ad37dc6e2bca025ba3436478b669
Peter Lieverdink <lieverdink@reliefweb.int>
2021-08-17 08:45:55 +1000

    [OPS-7078] Edgelord (#257)
    * [CLEANUP] We do not use PHP5.
    
    * [CLEANUP] So, also no derivative PHP5 images.
    
    * [CLEANUP] We all use base or k8s images, no special pupeteer, reliefweb or wordpress ones.
    
    * [CLEANUP] Remove unnecessary alpine-php subdirs.
    
    * [CLEANUP] Update the make includes after moving the Makefile.
    
    * [EDGELORD] Remove all overrides and install PHP from edge.
    
    * [EDGELORD] Initial attempt at PHP7.4 images. They do not work so well.
    
    * [OPS-7422] Always load xdebug last.
    
    * [OPS-7422] Add the (pecl) apcu and yaml PHP extensions.
    
    * [OOPS] Did not fix the merge conflict correctly.
    
    * [OPS-7078] Configure APCu and allow a few overrides.
    
    * [OPS-7078] Bump New Relic.
    
    * [OPS-7078] Unversion the image name, version the tag only.
    
    * [OPS-7078] Really add igbinary. Really remove wddx.
    
    * [OPS-7078] Build a single `php` image. Bump composer.
    
    * [OPS-7078] Ensure that the vendor bindir is in $PATH, for drush, you see.
    
    * [OPS-7078] Unify image names. Bump composer.
    
    * [OPS-7078] Rename the k8s image dirs, ensure we can build.
    
    * [OPS-7078] This is how we did it.
    
    * [OPS-7078] There is no need to suppress the resty warning, the suppression now generates a warning on ngixn 1.20.
    
    * [OPS-7078] Bake a new set of node images, used for the unified builder.
    
    * [HURRAY] We can add uploadprogress!
    
    * [HURRAY] We can add uploadprogress to 8!
    
    * [OPS-7078] Remove the wddx config file, there is no wddx module.

---
+++ build.sh
@@ -0,0 +1,108 @@
+#!/bin/bash -e
+
+BASE=3.14-202106-01
+VERSION=7.4.20-r0
+VERSION8=8.0.7-r0
+EXTRAVERSION=-202106-01
+
+# Is there a version?
+if [ -z "${VERSION}" ]; then
+  echo "The scripts require a non-empty version string."
+  exit 1
+fi
+
+# Is there an extra version?
+if [ -z "${EXTRAVERSION}" ]; then
+  echo "The scripts require a non-empty extraversion string."
+  exit 1
+fi
+
+# First off, we build the base php 7 image.
+pushd alpine-base-php/php7 && \
+  make VERSION=${VERSION} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${BASE} build && \
+  docker tag unocha/base-php:${VERSION}${EXTRAVERSION} unocha/base-php:7.4-develop && \
+  popd
+
+# Build the base php 7 image with NewRelic.
+pushd alpine-base-php/php7-newrelic && \
+  make VERSION=${VERSION} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION}${EXTRAVERSION} build && \
+  docker tag unocha/base-php:${VERSION}-NR${EXTRAVERSION} unocha/base-php:7.4-NR-develop && \
+  popd
+
+# Build the standard php 7 image.
+pushd alpine-php/php7 && \
+  make VERSION=${VERSION} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${VERSION}${EXTRAVERSION} build && \
+  docker tag unocha/php:${VERSION}${EXTRAVERSION} unocha/php:7.4-develop && \
+  docker tag unocha/php:${VERSION}${EXTRAVERSION} public.ecr.aws/unocha/php:${VERSION}${EXTRAVERSION} && \
+  docker tag unocha/php:${VERSION}${EXTRAVERSION} public.ecr.aws/unocha/php:7.4-develop && \
+  popd
+
+# Build the standard php 7 image with New Relic.
+pushd alpine-php/php7 && \
+  make VERSION=${VERSION} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION}-NR${EXTRAVERSION} build && \
+  docker tag unocha/php:${VERSION}-NR${EXTRAVERSION} unocha/php:7.4-NR-develop && \
+  docker tag unocha/php:${VERSION}-NR${EXTRAVERSION} public.ecr.aws/unocha/php:${VERSION}-NR${EXTRAVERSION} && \
+  docker tag unocha/php:${VERSION}-NR${EXTRAVERSION} public.ecr.aws/unocha/php:7.4-NR-develop && \
+  popd
+
+# Build the k8s php 7 image.
+pushd alpine-php/php-k8s-v7 && \
+  make VERSION=${VERSION} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${VERSION}${EXTRAVERSION} build && \
+  docker tag unocha/base-php:${VERSION}${EXTRAVERSION} unocha/php-k8s:7.4-develop && \
+  docker tag unocha/base-php:${VERSION}${EXTRAVERSION} public.ecr.aws/unocha/php-k8s:${VERSION}${EXTRAVERSION} && \
+  docker tag unocha/base-php:${VERSION}${EXTRAVERSION} public.ecr.aws/unocha/php-k8s:7.4-develop && \
+  popd
+
+# Build the k8s php 7 image with New Relic.
+pushd alpine-php/php-k8s-v7 && \
+  make VERSION=${VERSION} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION}-NR${EXTRAVERSION} build && \
+  docker tag unocha/php-k8s:${VERSION}-NR${EXTRAVERSION} unocha/php-k8s:7.4-NR-develop && \
+  docker tag unocha/php-k8s:${VERSION}-NR${EXTRAVERSION} public.ecr.aws/unocha/php-k8s:${VERSION}-NR${EXTRAVERSION} && \
+  docker tag unocha/php-k8s:${VERSION}-NR${EXTRAVERSION} public.ecr.aws/unocha/php-k8s:7.4-NR-develop && \
+  popd
+
+# First off, we build the base php 8 image.
+pushd alpine-base-php/php8 && \
+  make VERSION=${VERSION8} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${BASE} build && \
+  docker tag unocha/base-php:${VERSION8}${EXTRAVERSION} unocha/base-php:8.0-develop && \
+  popd
+
+# Build the base php 8 image with NewRelic.
+pushd alpine-base-php/php8-newrelic && \
+  make VERSION=${VERSION8} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION8}${EXTRAVERSION} build && \
+  docker tag unocha/base-php:${VERSION8}-NR${EXTRAVERSION} unocha/base-php:8.0-NR-develop && \
+  popd
+
+# Build the standard php 8 image.
+pushd alpine-php/php8 && \
+  make VERSION=${VERSION8} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${VERSION8}${EXTRAVERSION} build && \
+  docker tag unocha/php:${VERSION8}${EXTRAVERSION} unocha/php:8.0-develop && \
+  docker tag unocha/php:${VERSION8}${EXTRAVERSION} public.ecr.aws/unocha/php:${VERSION8}${EXTRAVERSION} && \
+  docker tag unocha/php:${VERSION8}${EXTRAVERSION} public.ecr.aws/unocha/php:8.0-develop && \
+  popd
+
+# Build the standard php 8 image with New Relic.
+pushd alpine-php/php8 && \
+  make VERSION=${VERSION8} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION8}-NR${EXTRAVERSION} build && \
+  docker tag unocha/php:${VERSION8}-NR${EXTRAVERSION} unocha/php:8.0-NR-develop && \
+  docker tag unocha/php:${VERSION8}-NR${EXTRAVERSION} public.ecr.aws/unocha/php:${VERSION8}-NR${EXTRAVERSION} && \
+  docker tag unocha/php:${VERSION8}-NR${EXTRAVERSION} public.ecr.aws/unocha/php:8.0-NR-develop && \
+  popd
+
+# Build the k8s php 8 image.
+pushd alpine-php/php-k8s-v8 && \
+  make VERSION=${VERSION8} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${VERSION8}${EXTRAVERSION} build && \
+  docker tag unocha/base-php:${VERSION8}${EXTRAVERSION} unocha/php-k8s:8.0-develop && \
+  docker tag unocha/base-php:${VERSION8}${EXTRAVERSION} public.ecr.aws/unocha/php-k8s:${VERSION8}${EXTRAVERSION} && \
+  docker tag unocha/base-php:${VERSION8}${EXTRAVERSION} public.ecr.aws/unocha/php-k8s:8.0-develop && \
+  popd
+
+# Build the k8s php 8 image with New Relic.
+pushd alpine-php/php-k8s-v8 && \
+  make VERSION=${VERSION8} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION8}-NR${EXTRAVERSION} build && \
+  docker tag unocha/php-k8s:${VERSION8}-NR${EXTRAVERSION} unocha/php-k8s:8.0-NR-develop && \
+  docker tag unocha/php-k8s:${VERSION8}-NR${EXTRAVERSION} public.ecr.aws/unocha/php-k8s:${VERSION8}-NR${EXTRAVERSION} && \
+  docker tag unocha/php-k8s:${VERSION8}-NR${EXTRAVERSION} public.ecr.aws/unocha/php-k8s:8.0-NR-develop && \
+  popd
+
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/unocha
