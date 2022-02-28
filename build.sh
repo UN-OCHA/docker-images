2af49c3799b5eb66dec4f178507d713ed16834f6
Peter Lieverdink <lieverdink@reliefweb.int>
2022-03-01 09:39:23 +1100

    [OPS-8096] New php & varnish (#294)
    * [OPS-8096] Use the latest v2.x of S6, so init does not change.
    
    * [OPS-8096] Build new php and varnish images; rename their build dirs, update the build script to match.
    
    * [OPS-8096] Also bake new unified builders to match PHP versions.
    
    * [OPS-8096] Use the correct version to tag the PHP8 builder.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-BASE=3.15-202201-01
-VERSION=7.4.27-r0
-VERSION8=8.0.14-r0
-EXTRAVERSION=-202201-01
+BASE=3.15-202202-01
+VERSION=7.4.28-r0
+VERSION8=8.0.16-r0
+EXTRAVERSION=-202202-01
 STABILITY=stable
 REGISTRY=public.ecr.aws/unocha
 
@@ -20,75 +20,88 @@ if [ -z "${EXTRAVERSION}" ]; then
 fi
 
 # First off, we build the base php 7 image.
-pushd alpine-base-php/php7 && \
+pushd php/base/php7 && \
   make VERSION=${VERSION} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${BASE} build && \
   docker tag ${REGISTRY}/base-php:${VERSION}${EXTRAVERSION} ${REGISTRY}/base-php:7.4-${STABILITY} && \
   popd
 
 # Build the base php 7 image with NewRelic.
-pushd alpine-base-php/php7-newrelic && \
+pushd php/base/php7-newrelic && \
   make VERSION=${VERSION} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION}${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/base-php:${VERSION}-NR${EXTRAVERSION} ${REGISTRY}/base-php:7.4-NR-${STABILITY} && \
   popd
 
 # Build the standard php 7 image.
-pushd alpine-php/php7 && \
+pushd php/php7 && \
   make VERSION=${VERSION} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${VERSION}${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/php:${VERSION}${EXTRAVERSION} ${REGISTRY}/php:7.4-stable && \
   popd
 
 # Build the standard php 7 image with New Relic.
-pushd alpine-php/php7 && \
+pushd php/php7 && \
   make VERSION=${VERSION} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION}-NR${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/php:${VERSION}-NR${EXTRAVERSION} ${REGISTRY}/php:7.4-NR-${STABILITY} && \
   popd
 
 # Build the k8s php 7 image.
-pushd alpine-php/php-k8s-v7 && \
+pushd php/php-k8s-v7 && \
   make VERSION=${VERSION} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${VERSION}${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/php-k8s:${VERSION}${EXTRAVERSION} ${REGISTRY}/php-k8s:7.4-${STABILITY} && \
   popd
 
 # Build the k8s php 7 image with New Relic.
-pushd alpine-php/php-k8s-v7 && \
+pushd php/php-k8s-v7 && \
   make VERSION=${VERSION} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION}-NR${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/php-k8s:${VERSION}-NR${EXTRAVERSION} ${REGISTRY}/php-k8s:7.4-NR-${STABILITY} && \
   popd
 
+# Build the php 7 builder image.
+pushd php/builder7 && \
+  make VERSION=${VERSION} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=14-alpine build && \
+  docker tag ${REGISTRY}/unified-builder:${VERSION}${EXTRAVERSION} ${REGISTRY}/unified-builder:7.4-${STABILITY} && \
+  popd
+
 # First off, we build the base php 8 image.
-pushd alpine-base-php/php8 && \
+pushd php/base/php8 && \
   make VERSION=${VERSION8} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${BASE} build && \
   docker tag ${REGISTRY}/base-php:${VERSION8}${EXTRAVERSION} ${REGISTRY}/base-php:8.0-${STABILITY} && \
   popd
 
 # Build the base php 8 image with NewRelic.
-pushd alpine-base-php/php8-newrelic && \
+pushd php/base/php8-newrelic && \
   make VERSION=${VERSION8} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION8}${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/base-php:${VERSION8}-NR${EXTRAVERSION} ${REGISTRY}/base-php:8.0-NR-${STABILITY} && \
   popd
 
 # Build the standard php 8 image.
-pushd alpine-php/php8 && \
+pushd php/php8 && \
   make VERSION=${VERSION8} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${VERSION8}${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/php:${VERSION8}${EXTRAVERSION} ${REGISTRY}/php:8.0-${STABILITY} && \
   popd
 
 # Build the standard php 8 image with New Relic.
-pushd alpine-php/php8 && \
+pushd php/php8 && \
   make VERSION=${VERSION8} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION8}-NR${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/php:${VERSION8}-NR${EXTRAVERSION} ${REGISTRY}/php:8.0-NR-${STABILITY} && \
   popd
 
 # Build the k8s php 8 image.
-pushd alpine-php/php-k8s-v8 && \
+pushd php/php-k8s-v8 && \
   make VERSION=${VERSION8} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=${VERSION8}${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/php-k8s:${VERSION8}${EXTRAVERSION} ${REGISTRY}/php-k8s:8.0-${STABILITY} && \
   popd
 
 # Build the k8s php 8 image with New Relic.
-pushd alpine-php/php-k8s-v8 && \
+pushd php/php-k8s-v8 && \
   make VERSION=${VERSION8} EXTRAVERSION=-NR${EXTRAVERSION} UPSTREAM=${VERSION8}-NR${EXTRAVERSION} build && \
   docker tag ${REGISTRY}/php-k8s:${VERSION8}-NR${EXTRAVERSION} ${REGISTRY}/php-k8s:8.0-NR-${STABILITY} && \
   popd
 
+# Build the php 8 builder image.
+pushd php/builder8 && \
+  make VERSION=${VERSION8} EXTRAVERSION=${EXTRAVERSION} UPSTREAM=16-alpine build && \
+  docker tag ${REGISTRY}/unified-builder:${VERSION8}${EXTRAVERSION} ${REGISTRY}/unified-builder:8.0-${STABILITY} && \
+  popd
+
+# Login, so we can push.
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/unocha
